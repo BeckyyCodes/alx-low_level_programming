2875f742f3dac1db2ade41f17ea0a7f2aca9ad75
rebekahho <l>
2022-10-14 21:40:01 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - ou are not allowed to use any variable of type char.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
