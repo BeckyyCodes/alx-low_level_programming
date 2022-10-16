2b0b05fcb08170b71848c929b08aadeb1158a6f0
rebekahho <l>
2022-10-16 00:29:10 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+
+/**
+ * main - prints various sizes.
+ *
+ * return: always 0.
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
